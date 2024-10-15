788bf07e483a38fe0a9d407fb67b0eeebfc09c37
Aaron Maraya <aaronmaraya7@gmail.com>
2024-10-15 14:35:10 +0800

    feat: create a function for burger menu.
    file :
    -assets/js/script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
-console.log("Hello world")
+function toggleMenu() {
+    const navMenu = document.getElementById('navMenu');
+    const burger = document.querySelector('.burger');
+    navMenu.classList.toggle('show');
+
+    // Change the icon
+    if (navMenu.classList.contains('show')) {
+        burger.innerHTML = '&times;'; // Change to 'X'
+    } else {
+        burger.innerHTML = '&#9776;'; // Change back to burger icon
+    }
+}
